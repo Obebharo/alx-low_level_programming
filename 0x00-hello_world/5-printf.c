e6725745fcb68cd9e44ec2b942a671e1821134f4
Igoni Obebharo <obebharoi@gmail.com>
2022-09-10 00:09:09 -0700

    print text using printf()

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Description: Prints the string text using printf() function
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
