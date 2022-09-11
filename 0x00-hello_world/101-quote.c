28161a7a3d6fb8d2afb8bc3b7129b1729c7b7fff
Igoni Obebharo <obebharoi@gmail.com>
2022-09-11 01:18:45 -0700

    Writing to standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main-program execution point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+	return (0);
