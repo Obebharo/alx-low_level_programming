21caebc7efbe459d63aaef245ae5a5bfc1703d18
Igoni Obebharo <obebharoi@gmail.com>
2022-09-11 19:30:25 -0700

    print all lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ *
+ * Description: This program prints all lower case alphabets with putchar()
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+
+	return (0);
+
+}
