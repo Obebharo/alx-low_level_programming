e7e06b5abb3b0205b8f8cc0a5a549dc6c32557ee
Igoni Obebharo <obebharoi@gmail.com>
2022-09-11 19:01:52 -0700

    generate random number, retrieve the last digit and print

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	if (last > 5)
 		printf("and is greater than 5\n");
 	else if (n == 0)
-		printf("and is 0\n");
+		printf("and is less than 6 and not 0\n");
 	else
 		printf("and is 0\n");
 
