cfc6b51a7bc7babfdd05f73694d5e452928e6656
Igoni Obebharo <obebharoi@gmail.com>
2022-09-14 14:10:06 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <main.h>
 
 /**
  * main - entry point of the program
@@ -10,7 +10,13 @@
 
 int main(void)
 {
-	printf("_putchar\n");
-
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,5 @@
+#ifndef ALX_TEST_HEADER
+#define ALX_TEST_HEADER
+
+int _putchar(char);
+#endif
