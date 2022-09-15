d72938cc0e1c2105ac9c40e7423fb4a0477b63a4
Igoni Obebharo <obebharoi@gmail.com>
2022-09-14 23:25:12 -0700

    checks for lower case alphabets

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,7 @@
 /**
  * _islower - identifies lower case characters.
  * @i: the variable that recieves the character to be checked.
+ * Return: 1 if it is a lower case and 0 otherwise
  */
 int _islower(int i)
 {
