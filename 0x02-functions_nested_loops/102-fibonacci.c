af0f47469454b2a0320054f8e2d3313b2676ad8b
Igoni Obebharo <obebharoi@gmail.com>
2022-09-15 23:50:41 -0700

    print fibonacci sequence including 1 and 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Description: Print the first 50 fibonacci numbers, starting with 1 and 2.
+ * Numbers must be coma and space separated.
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 3;
+	long int first = 1, second = 2;
+	long int next = first + second;
+
+	printf("%lu, ", first);
+	printf("%lu, ", second);
+
+	while (num <= 50)
+	{
+		if (num == 50)
+			printf("%lu \n", next);
+		else
+			printf("%lu, ", next);
+
+		first = second;
+		second = next;
+		next = first + second;
+		num++;
+	}
+	return (0);
+}
