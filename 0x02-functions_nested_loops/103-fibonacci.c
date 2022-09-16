f1053a84d48b4bfbc3708f550060627e41fae1a9
Igoni Obebharo <obebharoi@gmail.com>
2022-09-16 00:13:07 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 1, j = 1, sum = 0;
+
+	while (i < 4000000)
+	{
+		i = i + j;
+		j = i - j;
+
+		if (i % 2 == 0)
+			sum += i;
+	}
+
+	printf("%d", sum);
+	return (0);
+}
