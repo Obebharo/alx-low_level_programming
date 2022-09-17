1f15578787617037a009e2ffbf47f379dc7a14ae
Igoni Obebharo <obebharoi@gmail.com>
2022-09-17 01:24:20 -0700

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@
 
 void print_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 48; i <= 57; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == 50 || i == 52)
+		if (i == '0' || i == '9')
 			continue;
 		_putchar(i);
 	}
