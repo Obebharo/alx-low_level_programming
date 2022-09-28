30bed3ddbaecb9a375f2529bae95cf2616f9e9cb
Igoni Obebharo <obebharoi@gmail.com>
2022-09-28 05:17:07 -0700

    emulate puts using recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,14 +3,17 @@
 /**
  * _puts_recursion - function to emulate puts()
  * @s: pointer to the string
+ *
+ * Return: void
  */
 
 void _puts_recursion(char *s)
 {
+	_putchar(s[0]);
+
 	if (s[1] != '\0')
 	{
-		_putchar(s[0]);
-		_puts_recursion(s + i);
+		_puts_recursion(s + 1);
 	}
 	else
 	{
