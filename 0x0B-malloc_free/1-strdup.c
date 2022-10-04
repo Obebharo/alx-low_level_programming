7f2229a01a358126443ed93bed3ae99af7b1e0d5
Igoni Obebharo <obebharoi@gmail.com>
2022-10-04 13:30:04 -0700

    task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include<main.h>
+
+/**
+ * _strdup - function that returns copy of the string used as argument.
+ * @str: pointer to the string
+ * Return: pointer 
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	for (; str[len] != '\0'; len++)
+		;
+	char *arr = malloc(len * sizeof(char));
+
+	if (arr == 0)
+		return (NULL);
+	else
+	{
+		for (; i<size; i++)
+		       arr[i] = str[i];
+	}
+	return (arr);
+}
