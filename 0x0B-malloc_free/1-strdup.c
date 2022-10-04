5a363f1b4866534b61f5351c8e6cacc389467aaf
Igoni Obebharo <obebharoi@gmail.com>
2022-10-04 15:11:12 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,30 +1,34 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _strdup - function that returns copy of the string used as argument.
- * @str: pointer to the string
- * Return: pointer 
+ *_strdup - returns a pointer to a copy of string
+ *
+ *@str:string parameter
+ *
+ *Return:NULL(fail),pointer
  */
-
 char *_strdup(char *str)
 {
-	int i = 0;
-	int len = 0;
+	int i = 0, len = 0;
+	char *s;
 
 	if (str == NULL)
-		return (NULL);
-	for (; str[len] != '\0'; len++)
-		;
-	char *arr = malloc(len * sizeof(char));
-
-	if (arr == 0)
-		return (NULL);
-	else
 	{
-		for (; i<size; i++)
-		       arr[i] = str[i];
+		return (NULL);
 	}
-	return (arr);
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	s = malloc(sizeof(char) * (len + 1));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	{
+		s[i] = str[i];
+	}
+	s[len] = '\0';
+	return (s);
 }
